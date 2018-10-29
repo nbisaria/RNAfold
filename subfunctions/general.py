5b195fe60eba75925943d6f1c252987fd2db9384
Namita Bisaria <nbisaira@gmail.com>
2018-10-29 11:41:51 -0400

    made changes to general script to include PP7 hairpin and constrained SS

--- subfunctions/general.py
+++ subfunctions/general.py
@@ -3,7 +3,10 @@
 
 import RNA
 import subprocess
+import pandas as pd
+import imp
 from random import random
+imp.load_source("walkerrandom",("/lab/bartel1_ata/nbisaria/RNAfold/subfunctions/walkerrandom.py"))
 from walkerrandom import Walkerrandom
 from collections import defaultdict
 
@@ -11,8 +14,9 @@ from collections import defaultdict
 
 
 def GetMotifstoFilter():
-    splicesites = ['AAUAA','GGUAAGU']
-    seqs = splicesites
+    splicesites = ['GGUAAGU']
+    polyA = ['AAUAA']
+    seqs = splicesites + polyA
     return seqs
 
 class Parameters():
@@ -31,14 +35,21 @@ def GetdG_dotbracket(seq):
     return dG, dotbracket
 
 def Get_MS2():
-    MS2_seq = 'gcgcACATGAGGATCACCCATGTgc' #extended hp
+    MS2_seq = 'gcgcACATGAGGATCACCCATGTgc' #extended hp, from zalatan et al.
+    dG, dotbracket = GetdG_dotbracket(MS2_seq)
+    print(dG, dotbracket, MS2_seq)
     return MS2_seq
+def Get_PP7():
+    PP7 = 'aacaTAAGGAGTTTATATGGAAACCCTTAtg' #from zalatan et al.
+    dG, dotbracket = GetdG_dotbracket(PP7)
+    print(dG, dotbracket, PP7)
+    return PP7
 
 def GenerateRandomScaffAndLinker(n):
     per_AU_scaff = 0.65
     per_AU_linker = 0.85 
     len_scaff = 10
-    len_linker = 3
+    len_linker = 2
     Scaffprob = dict( A=per_AU_scaff/2, U=per_AU_scaff/2, C=(1-per_AU_scaff)/2, G=(1-per_AU_scaff)/2 )
     srand = Walkerrandom( Scaffprob.values(), Scaffprob.keys() )
     Linkprob = dict( A=per_AU_linker/2, U=per_AU_linker/2, C=(1-per_AU_linker)/2, G=(1-per_AU_linker)/2 )
@@ -51,7 +62,9 @@ def GenerateRandomScaffAndLinker(n):
         for i in range(0,l):
             s = s + wrand.random()
         return s
-    
+    MS2 = Get_MS2()
+    PP7 = Get_PP7()
+    db_final = '....(((((((.((((......)))))))))))..........................(((((((.((....)))))))))..'
     while numhits < n:
         l1 = makeSeqwProb(len_linker, lrand)
         l2 = makeSeqwProb(len_linker, lrand)
@@ -59,18 +72,18 @@ def GenerateRandomScaffAndLinker(n):
         l4 = makeSeqwProb(len_linker, lrand)
         s1 = makeSeqwProb(len_scaff, srand)
         s2 = makeSeqwProb(len_scaff, srand)
-        MS2 = Get_MS2()
-        seq_final =  l1+MS2 + l2 +s1 + s2 + l3 + MS2 + l4 
+
+        seq_final =  l1+PP7 + l2 +s1 + s2 + l3 + MS2 + l4 
         dG, db = GetdG_dotbracket(seq_final)
         constrain = db[0:3] + db[11:15] + db[22:25] + db[-3:] + db[-26:-22] + db[-14:-10]
         constrain = db[0:3] + db[11:15] + db[-3:] + db[-14:-10]
-        # print(dG,dotbracket)
+        # print(dG,db)
         # print(db)
-        if (dG > -14.8) and ('(' not in constrain) and (')' not in constrain) and not any(motif in seq_final  for motif in seqsfilter):
+        if (dG > -21) and (db == db_final) and not any(motif in seq_final  for motif in seqsfilter):
             print(seq_final)
             print(dG, db)
             numhits = numhits + 1
-            contexts_final['c' + str(numhits)] = [l1,l2, MS2, s1, s2, MS2, l3, l4]
+            contexts_final['c' + str(numhits)] = [l1,PP7, l2, s1, s2, l3, MS2, l4]
     return contexts_final
 
 def main():
